dcb81f82795d505be8003dec301e5e312a5c4269
olzhas27 <nuralinov_ozh@mail.ru>
2021-02-27 01:41:12 +0300

    migrated

---
+++ src/test/java/com/github/olzhas27/tic/tac/server/TicTacServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.olzhas27.tic.tac.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
